dcaa2d94ba91453361e1bb7a840f81aec1f54275
harpercham <maxchao7023@gmail.com>
2020-09-23 15:52:08 +0800

    initing

--- routes/index.js
+++ routes/index.js
@@ -124,7 +124,7 @@ function reject(auth) {
 // Load client secrets from a local file.
 var checkID;
 router.post('/check', function (req, res, next) {
-  checkID = ((req.body).form_data);
+  checkID = (req.body).fname;
   res.redirect('/');
 });
 
