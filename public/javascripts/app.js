29d160270a81d18ce5432920dc91071b3f4ba3d7
harpercham <maxchao7023@gmail.com>
2020-09-23 15:01:59 +0800

    initing2

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -5,7 +5,7 @@ $("form").submit(function(e){
   e.stopImmediatePropagation();
 
     var form_data = ($(this).serializeArray()[0].value);
-
+  alert(form_data)
         //ajax post
         $.ajax({
             type: "POST",
